ee5ae957f7a0b009490446ae47e64e04bd96ea41
Tarun Rana <tarun@91mobiles.com>
2026-05-21 10:54:26 +0530

    post request

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+app = FastAPI(title='My FastAPI Application', description='This is a sample FastAPI application.', version='1.0.0')
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.model_dump()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
